68faaf4cb94973f632435fe8e15c6238df45249d
Avasam <samuel.06@hotmail.com>
2022-07-19 16:36:28 -0400

    Reactivated CIs

--- scripts/install.ps1
+++ scripts/install.ps1
@@ -5,12 +5,14 @@ If ($IsWindows) {
   pip install -r "$PSScriptRoot/requirements$dev-win32.txt"
 }
 ElseIf ($IsLinux) {
-  # Required for splash screen
-  sudo apt-get install python3-tk
-  # Helps ensure the CI machine has the required libraries for PyQt6.
-  # Not everything here is required, but using the documentation from
-  # https://wiki.qt.io/Building_Qt_5_from_Git#Libxcb
-  sudo apt-get install '^libxcb.*-dev' libx11-xcb-dev libglu1-mesa-dev libxrender-dev libxi-dev libxkbcommon-dev libxkbcommon-x11-dev
+  if (-not $env:GITHUB_JOB -or $env:GITHUB_JOB -eq 'Build') {
+    # Required for splash screen
+    sudo apt-get install python3-tk
+    # Helps ensure build machine has the required PyQt6 libraries for all target machines.
+    # Not everything here is required, but using the documentation from
+    # https://wiki.qt.io/Building_Qt_5_from_Git#Libxcb
+    sudo apt-get install '^libxcb.*-dev' libx11-xcb-dev libglu1-mesa-dev libxrender-dev libxi-dev libxkbcommon-dev libxkbcommon-x11-dev
+  }
   pip install -r "$PSScriptRoot/requirements$dev-linux.txt"
 }
 Else {
