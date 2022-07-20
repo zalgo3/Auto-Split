9048dbf1df1a7dc337896b1f355c953ae9bc5fad
Avasam <samuel.06@hotmail.com>
2022-07-19 23:26:23 -0400

    sudo pip install on CI

--- scripts/install.ps1
+++ scripts/install.ps1
@@ -35,7 +35,13 @@ ElseIf ($IsLinux) {
     sudo apt-get install '^libxcb.*-dev' libx11-xcb-dev libglu1-mesa-dev libxrender-dev libxi-dev libxkbcommon-dev libxkbcommon-x11-dev
   }
 
-  pip install -r "$PSScriptRoot/requirements$dev-linux.txt"
+  If ($env:GITHUB_JOB) {
+    # Can't set groups as above on CI
+    sudo pip install -r "$PSScriptRoot/requirements$dev-linux.txt"
+  }
+  Else {
+    pip install -r "$PSScriptRoot/requirements$dev-linux.txt"
+  }
 }
 Else {
   pip install -r "$PSScriptRoot/requirements$dev.txt"
