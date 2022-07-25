f011e50e0c4fb7f8ac98e721708dc7e9c932bdca
Avasam <samuel.06@hotmail.com>
2022-07-24 21:36:52 -0400

    Fixed linting

--- scripts/install.ps1
+++ scripts/install.ps1
@@ -55,6 +55,6 @@ If ($dev) {
 
 # Only the Pyright job and local devs have node installed
 if (-not $env:GITHUB_JOB -or $env:GITHUB_JOB -eq 'Pyright') {
-  npm install --location=global pyright@latest
-  npm list --location=global pyright
+  npm install --global pyright@latest
+  npm list --global pyright
 }
